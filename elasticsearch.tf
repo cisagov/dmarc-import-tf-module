050aaae95299764f0b3daa3c544ac300d07b1f60
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-06-05 10:34:03 -0400

    Adding a policy document to Elasticsearch so that the reporter user can query the database

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -20,3 +20,41 @@ resource "aws_elasticsearch_domain" "es" {
 
   tags = "${var.tags}"
 }
+
+# IAM policy document that that allows unauthenticated access to the
+# Elasticsearch domain from the CAL VPN.
+data "aws_iam_policy_document" "cal_vpn_es_doc" {
+  statement {
+    effect = "Allow"
+    
+    actions = [
+      "es:ESHTTPGet"
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "${aws_iam_user.reporter_user.arn}"
+      ]
+    }
+
+    # condition {
+    #   test = "IpAddress"
+    #   variable = "aws:SourceIp"
+    #   values = [
+    #     "X.Y.Z.T/32"
+    #   ]
+    # }
+
+    resources = [
+      "${aws_elasticsearch_domain.es.arn}/*"
+    ]
+  }
+}
+
+# The access policy for the Elasticsearch domain
+resource "aws_elasticsearch_domain_policy" "cal_vpn" {
+  domain_name = "${aws_elasticsearch_domain.es.domain_name}"
+
+  access_policies = "${data.aws_iam_policy_document.cal_vpn_es_doc.json}"
+}
