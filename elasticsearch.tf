393701ac9a72ddc6f00c76313790ccf624f9eb10
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-12-13 11:55:06 -0500

    Add application logs to ES instance, for warnings and errors

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -1,3 +1,37 @@
+# The CloudWatch log group where application logs will be written
+resource "aws_cloudwatch_log_group" "es_logs" {
+  name = "/aws/aes/domains/${var.elasticsearch_domain_name}"
+  retention_in_days = 30
+
+  tags = "${var.tags}"
+}
+
+# IAM policy document that that allows ES to write to CloudWatch logs
+data "aws_iam_policy_document" "es_cloudwatch_doc" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = ["es.amazonaws.com"]
+    }
+
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogStream"
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.es_logs.arn}"
+    ]
+  }
+}
+
+resource "aws_cloudwatch_log_resource_policy" "es_cloudwatch_policy" {
+  policy_name = "dmarc-import-es-cloudwatch-policy"
+  policy_document = "${data.aws_iam_policy_document.es_cloudwatch_doc.json}"
+}
+
 # The Elasticsearch domain
 resource "aws_elasticsearch_domain" "es" {
   domain_name = "${var.elasticsearch_domain_name}"
@@ -16,6 +50,11 @@ resource "aws_elasticsearch_domain" "es" {
 
   encrypt_at_rest = {
     enabled = true
+  }
+
+  log_publishing_options {
+    cloudwatch_log_group_arn = "${aws_cloudwatch_log_group.es_logs.arn}"
+    log_type = "ES_APPLICATION_LOGS"
   }
 
   tags = "${var.tags}"
