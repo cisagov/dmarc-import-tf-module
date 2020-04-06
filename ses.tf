230ff6078c45081b6168af8993e17a2959ab0a4e
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-06 15:03:18 -0400

    Promote the rule set name to a full-fledged variable

--- ses.tf
+++ ses.tf
@@ -1,19 +1,14 @@
-# Stash the name of our rule set, so it is defined in only one place
-locals {
-  rule_set_name = "dmarc-import-rules"
-}
-
 # Make a new rule set for handling the DMARC aggregate report emails
 # that arrive
 resource "aws_ses_receipt_rule_set" "rules" {
-  rule_set_name = local.rule_set_name
+  rule_set_name = var.rule_set_name
 }
 
 # Make a rule for handling the DMARC aggregate report emails that
 # arrive
 resource "aws_ses_receipt_rule" "rule" {
   name          = "receive-dmarc-emails"
-  rule_set_name = local.rule_set_name
+  rule_set_name = var.rule_set_name
   recipients    = var.emails
 
   enabled      = true
@@ -34,5 +29,5 @@ resource "aws_ses_receipt_rule" "rule" {
 
 # Make this rule set the active one
 resource "aws_ses_active_receipt_rule_set" "active" {
-  rule_set_name = local.rule_set_name
+  rule_set_name = var.rule_set_name
 }
