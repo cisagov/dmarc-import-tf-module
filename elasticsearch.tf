d55134f1a33cd457913d472cbadab6080e274161
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-12-22 16:26:50 -0500

    Enable Cognito for Elasticsearch and define access policy

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -31,10 +31,41 @@ resource "aws_cloudwatch_log_resource_policy" "es_cloudwatch_policy" {
   policy_name     = "dmarc-import-es-cloudwatch-policy"
 }
 
+# Policy document that allows authenticated Cognito users access to the
+# Elasticsearch domain
+data "aws_iam_policy_document" "es_cognito_auth" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "es:*",
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        aws_iam_role.cognito_authenticated.arn,
+      ]
+    }
+
+    resources = [
+      "arn:aws:es:${var.aws_region}:*:domain/${var.elasticsearch_domain_name}/*",
+    ]
+  }
+}
+
 # The Elasticsearch domain
 resource "aws_elasticsearch_domain" "es" {
+  access_policies       = data.aws_iam_policy_document.es_cognito_auth.json
   domain_name           = var.elasticsearch_domain_name
   elasticsearch_version = "OpenSearch_1.3"
+
+  cognito_options {
+    enabled          = true
+    identity_pool_id = aws_cognito_identity_pool.dmarc.id
+    role_arn         = aws_iam_role.opensearch_cognito.arn
+    user_pool_id     = aws_cognito_user_pool.dmarc.id
+  }
 
   cluster_config {
     instance_type  = "m6g.large.elasticsearch"
