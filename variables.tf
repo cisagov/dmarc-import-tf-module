805d9ba8da1959a9b88ee9277debffe5d924ba1f
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-12-22 16:14:22 -0500

    Add optional variables related to Cognito

--- variables.tf
+++ variables.tf
@@ -65,3 +65,45 @@ variable "aws_region" {
   description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
 }
+
+variable "cognito_authenticated_role_name" {
+  default     = "dmarc-import-authenticated"
+  description = "The name of the IAM role that grants authenticated access to the Elasticsearch database."
+  type        = string
+}
+
+variable "cognito_identity_pool_name" {
+  default     = "dmarc-import"
+  description = "The name of the Cognito identity pool to use for access to the Elasticsearch database."
+  type        = string
+}
+
+variable "cognito_user_pool_client_name" {
+  default     = "dmarc-import"
+  description = "The name of the Cognito user pool client to use for access to the Elasticsearch database."
+  type        = string
+}
+
+variable "cognito_user_pool_domain" {
+  default     = "dmarc-import"
+  description = "The domain to use for the Cognito endpoint. For custom domains, this is the fully-qualified domain name, such as auth.example.com. For Amazon Cognito prefix domains, this is the prefix alone, such as auth."
+  type        = string
+}
+
+variable "cognito_user_pool_name" {
+  default     = "dmarc-import"
+  description = "The name of the Cognito user pool to use for access to the Elasticsearch database."
+  type        = string
+}
+
+variable "cognito_usernames" {
+  default     = {}
+  description = "A map whose keys are the usernames of each Cognito user and whose values are a map containing supported user attributes.  The only currently-supported attribute is \"email\" (string).  Example: { \"firstname1.lastname1\" = { \"email\" = \"firstname1.lastname1@foo.gov\" }, \"firstname2.lastname2\" = { \"email\" = \"firstname2.lastname2@foo.gov\" } }"
+  type        = map(object({ email = string }))
+}
+
+variable "opensearch_service_role_for_auth_name" {
+  default     = "opensearch-service-cognito-access"
+  description = "The name of the IAM role that gives Amazon OpenSearch Service permissions to configure the Amazon Cognito user and identity pools and use them for OpenSearch Dashboards/Kibana authentication."
+  type        = string
+}
